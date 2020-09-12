85dc6667d64b5a5f814f994a2172f1c75f4205dd
Farrid Kuntoro <farridkuntoro@mahasiswa.unikom.ac.id>
2020-09-12 14:50:03 +0700

    Sidebar has a going but is not bad 🤷‍♂️

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import "./Sidebar.css";
 import SidebarRow from "./SidebarRow";
+import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
+import EmojiFlagsIcon from '@material-ui/icons/EmojiFlags';
+import PeopleIcon from '@material-ui/icons/People';
+import ChatIcon from '@material-ui/icons/Chat';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import { ExpandMoreOutlined } from '@material-ui/icons';
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <SidebarRow title="Pages"/>
-            <SidebarRow title="Friends"/>
+            <SidebarRow src='https://avatars3.githubusercontent.com/u/50895949?s=460&u=4566407f143de04afa1cba038d735b46e64a94ef&v=4' title='Farrid Kuntoro' />
+            <SidebarRow Icon={LocalHospitalIcon} title="COVID-19 Information Center" />
+            <SidebarRow Icon={EmojiFlagsIcon} title="Pages"/>
+            <SidebarRow Icon={PeopleIcon} title="Friends"/>
+            <SidebarRow Icon={ChatIcon} title="Messenger"/>
+            <SidebarRow Icon={StorefrontIcon} title="Marketplace"/>
+            <SidebarRow Icon={VideoLibraryIcon} title="Videos"/>
+            <SidebarRow Icon={ExpandMoreOutlined} title="Marketplace"/>
         </div>
     )
 }
--- src/SidebarRow.js
+++ src/SidebarRow.js
@@ -1,10 +1,14 @@
+import { Avatar } from '@material-ui/core';
 import React from 'react'
 import "./SidebarRow.css";
 
 function SidebarRow({ src, Icon, title }) {
     return (
         <div className="sidebarRow">
-            <p>{title}</p>
+            {src && <Avatar src={src} />}
+            {Icon && <Icon />}
+
+            <h4>{title}</h4>
         </div>
     )
 }
