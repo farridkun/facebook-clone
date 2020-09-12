41f3591030014ced8b9b000a0f8e2e7e5c135bdf
Farrid Kuntoro <farridkuntoro@mahasiswa.unikom.ac.id>
2020-09-12 14:30:52 +0700

    Typo on header__right

--- src/Header.js
+++ src/Header.js
@@ -42,7 +42,7 @@ function Header() {
                 </div>
             </div>
 
-            <div className="header_right">
+            <div className="header__right">
                 <div className="header__info">
                     <Avatar />
                     <h4>farrid_jr</h4>
