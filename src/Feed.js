d2c1624536d582c70744f76891e5694d4ebb8d0e
Farrid Kuntoro <farridkuntoro@mahasiswa.unikom.ac.id>
2020-09-12 14:55:01 +0700

    Made a new component for SnapStory VIews

--- src/Feed.js
+++ src/Feed.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import "./Feed.css";
+import StoryReel from './StoryReel';
 
 function Feed() {
     return (
         <div className="feed">
-            
+            <StoryReel />
+            {/* StoryReel */}
+            {/* MessageSender */}
         </div>
     )
 }
---
+++ src/Story.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Story.css";
+
+function Story() {
+    return (
+        <div className="story">
+            
+        </div>
+    )
+}
+
+export default Story
